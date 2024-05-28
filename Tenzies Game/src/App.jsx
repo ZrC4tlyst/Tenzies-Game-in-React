245363a6b736fc7f6e19f3d94d88910ad244323b
Zuhair Rehman <zuhair.rehman85@gmail.com>
2024-05-28 18:32:45 +0100

    Manually added 10  Die components  and applied styles

--- Tenzies Game/src/App.jsx
+++ Tenzies Game/src/App.jsx
@@ -5,8 +5,18 @@ function App() {
   return (
     <>
       <main className="main">
-        <h1>Tenzies</h1>
-        <Die diceValue={1} />
+        <div className="die-container">
+          <Die diceValue={1} />
+          <Die diceValue={2} />
+          <Die diceValue={3} />
+          <Die diceValue={4} />
+          <Die diceValue={5} />
+          <Die diceValue={6} />
+          <Die diceValue={1} />
+          <Die diceValue={2} />
+          <Die diceValue={3} />
+          <Die diceValue={4} />
+        </div>
       </main>
     </>
   );
